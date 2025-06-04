13243f90539c05f000033be995133097b30c6970
Weather Report <130683400+weather-report-op@users.noreply.github.com>
2025-06-04 10:11:32 -0600

    Fix storage bucket domain

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -4,7 +4,8 @@ const firebaseConfig = {
   apiKey: "AIzaSyCUWEE1evYM7ekFFJLK6fMlek0A-l-7X28",
   authDomain: "melomerezco-8fdfa.firebaseapp.com",
   projectId: "melomerezco-8fdfa",
-  storageBucket: "melomerezco-8fdfa.firebasestorage.app",
+  // The storage bucket URL should use the "appspot.com" domain
+  storageBucket: "melomerezco-8fdfa.appspot.com",
   messagingSenderId: "21672363858",
   appId: "1:21672363858:web:c4abf1810c3cf578449c8b",
   measurementId: "G-JP65778861"
